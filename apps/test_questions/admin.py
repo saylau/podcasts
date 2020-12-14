d2176a86b9944563d49e45f1c7fa88d7f5ef3d19
gglassota <73990775+gglassota@users.noreply.github.com>
2020-12-14 14:07:32 +0600

    Update admin.py

--- apps/test_questions/admin.py
+++ apps/test_questions/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import TestQuestion
+from .models import TestQuestion, Test
 
-
+@admin.register(Test)
 @admin.register(TestQuestion)
 class TestQuestionAdmin(admin.ModelAdmin):
     pass
