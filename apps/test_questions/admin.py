6ee99c39dccb9664fcff1a8d28fb8b7f9dc0c298
Zhanibek <gglassota2@gmail.com>
2020-12-15 14:50:13 +0600

    rm register test

--- apps/test_questions/admin.py
+++ apps/test_questions/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 
 from .models import TestQuestion, Test
 
-@admin.register(Test)
 @admin.register(TestQuestion)
 class TestQuestionAdmin(admin.ModelAdmin):
     pass
