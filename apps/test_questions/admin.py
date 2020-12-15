02cb13b3b54f8ad3fe2b719cb62bd462080092a9
root <root@server.sexedu.kz>
2020-12-15 14:19:47 +0600

    changes from server

--- apps/test_questions/admin.py
+++ apps/test_questions/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 
-from .models import TestQuestion
+from .models import TestQuestion, Test
 
 
 @admin.register(TestQuestion)
 class TestQuestionAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
     pass
