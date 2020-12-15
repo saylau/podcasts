a8235e5d88bdf20c4d67a64cd47450e3b05b1fc1
Zhanibek <gglassota2@gmail.com>
2020-12-15 15:20:18 +0600

    notifications

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, Notification
 
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
+    pass
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
     pass
