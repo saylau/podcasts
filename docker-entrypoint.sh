3ccef6230d0be1057934aafe31c5410c698ec05d
Zhanibek <gglassota2@gmail.com>
2020-12-15 16:43:34 +0600

    questions

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,7 @@ if [ $container_type = "CELERY" ]; then
 
 else
 
+  python manage.py makemigrations
   python manage.py migrate --noinput
   gunicorn --bind 0.0.0.0:8000 --access-logfile - config.wsgi:application
 fi;
