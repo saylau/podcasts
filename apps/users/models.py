fc66fd0a713eefba57def9455a879ab33dae3b7d
Zhanibek <gglassota2@gmail.com>
2020-12-15 15:17:51 +0600

    notifications

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,13 +5,34 @@ from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
-
+from fcm_django.models import FCMDevice
 
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return self.username
+
+class Notification(models.Model):
+    title = models.CharField(max_length=250, verbose_name='Заголовок')
+    body = models.CharField(max_length=250, verbose_name='Текст')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Уведомление'
+        verbose_name_plural = 'Уведомления'
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            device = FCMDevice.objects.all()
+            data = {
+                'title': notification.title,
+                'body': notification.body
+            }
+            device.send_message(title=notification.title, body=notification.body, data=data, sound="default")
+        super(Notification, self).save(*args, **kwargs)
 
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
