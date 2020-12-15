b390f39f118cc5441a136d7e7d34eede7259bb09
Zhanibek <gglassota2@gmail.com>
2020-12-15 16:02:49 +0600

    contacts

--- apps/contact/views.py
+++ apps/contact/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import City
 from .serializers import ContactCitySerializer
@@ -10,3 +11,4 @@ class ContactViewSet(ListAPIView):
     queryset = City.objects.all()
     serializer_class = ContactCitySerializer
     permission_classes = (AllowAny, )
+    pagination_class = LimitOffsetPagination
